8394602f469a9a0bd915df62809fd68b81ac0180
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-05-09 11:13:15 -0600

    AddIdea file

---
+++ src/Components/AddIdea.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class AddIdea extends Component {
+    constructor() {
+        super()
+        this.state = {
+            ideaName: '',
+            cost: null
+        }
+    }
+
+    handleUpdateInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleAddIdea = (e) => {
+        e.preventDefault()
+        axios.post('/api/addIdea', {
+            name: this.state.ideaName,
+            cost: this.state.cost
+        })
+        .then((res) => {
+            console.log(res)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Add Idea</h1>
+                <form onSubmit={this.handleAddIdea}>
+                <input
+                    placeholder='Idea Name'
+                    name='ideaName'
+                    onChange={this.handleUpdateInput}
+                />
+                <input
+                    placeholder='Cost'
+                    name='cost'
+                    onChange={this.handleUpdateInput}
+                    />
+                    <button>Add Event</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddIdea
